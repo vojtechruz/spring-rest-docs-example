be59eaa5d06468c2e234206048e6b3f247662869
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-05-24 18:00:18 +0200

    Initial commit

--- src/test/java/com/vojtechruzicka/springrestdocs/controllers/PersonControllerJunit4Test.java
+++ src/test/java/com/vojtechruzicka/springrestdocs/controllers/PersonControllerJunit4Test.java
@@ -1,15 +1,19 @@
 package com.vojtechruzicka.springrestdocs.controllers;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,16 +27,22 @@ public class PersonControllerJunit4Test {
 
     private MockMvc mockMvc;
 
+    @Rule
+    public JUnitRestDocumentation jUnitRestDocumentation = new JUnitRestDocumentation();
+
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(documentationConfiguration(this.jUnitRestDocumentation))
+                .build();
     }
 
     @Test
     public void getPersonShouldReturnOk() throws Exception {
         this.mockMvc.perform(get("/persons/"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"));
+                .andExpect(content().contentType("application/json;charset=UTF-8")).andDo(document("persons"));
     }
 
 }
